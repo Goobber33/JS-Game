f0b689b4c5f2833561c4a8dd13ba06953e78bfac
Kyle <kcparks1234@gmail.com>
2023-12-19 18:15:50 -0800

    Added running and sitting states

--- player.js
+++ player.js
@@ -1,5 +1,7 @@
+import { Sitting, Running } from './playerStates.js'
+
 export class Player {
-    constructor(game){
+    constructor(game) {
         this.game = game;
         this.width = 100;
         this.height = 91.3;
@@ -8,10 +10,19 @@ export class Player {
         this.vy = 0;
         this.weight = 1;
         this.image = document.getElementById('player');
+        this.frameX = 0;
+        this.frameY = 0;
         this.speed = 0;
         this.maxSpeed = 10;
+        this.states = [new Sitting(this), new Running(this)];
+        this.currentState = this.states[0];
+        this.currentState.enter();
+
     }
+
     update(input) {
+
+        this.currentState.handleInput(input);
 
         // Horizontal Movement
 
@@ -24,18 +35,24 @@ export class Player {
 
         // Verticial Movement
 
-        if (input.includes('ArrowUp') && this.onGround()) this.vy -= 10;
+        if (input.includes('ArrowUp') && this.onGround()) this.vy -= 30;
         this.y += this.vy;
         if (!this.onGround()) this.vy += this.weight;
         else this.vy = 0;
 
     }
-    draw(context){
-        context.drawImage(this.image, 0, 0, this.width, this.height, this.x, this.y, this.width, this.height);
+
+    draw(context) {
+        context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
     }
 
     onGround() {
         return this.y >= this.game.height - this.height;
+    }
+
+    setState(state) {
+        this.currentState = this.states[state];
+        this.currentState.enter()
     }
 
 }
